ee956b847d1f0448cc49ef3b8de5465b11a5284c
Danielaperez18 <160556551+Danielaperez18@users.noreply.github.com>
2024-03-01 23:19:54 -0600

    primer_commit

---
+++ Untitled1.cpp
@@ -0,0 +1,15 @@
+ #include <iostream>
+ using namespace std;
+ main (){ 
+ 	// i++ , i+=1 , i=i+1
+ 	int i =0;
+ 	int contar=0;
+    for (i=4;i<=8;i+=2){ // 4 hasta 7 ; 4 hasta 8
+        cout<<i<<endl;
+ 	    contar++;
+ 	
+	}
+	 cout<<"i fuera: "<<i<<endl;
+	 cout<<"ciclos: "<<contar<<endl;
+	 system("pause");
+}
